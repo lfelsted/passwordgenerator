3c1290f4e946c731fcf0024dcda6b1d2993d80ce
lfelsted <logansfelsted@gmail.com>
2020-10-29 14:24:05 -0600

    added comments to the code

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-//This is when i reset
+// declaring variables
 var capitalLetters = null;
 var lowerCase = null;
 var includeNumbers = null;
@@ -12,8 +12,9 @@ var numbersOp = "0123456789";
 var specialCharOp = " !\"#$%^&*()_+=-;:\\'?>,.~`|][}{";
 var options = [];
 var password = "";
-
+//setting perameters and criteria for password
 function generatePassword() {
+ //figuring out how long to set password length
   while (true) {
     password_length = parseInt(
       prompt(
@@ -28,19 +29,19 @@ function generatePassword() {
       break;
     }
   }
-
+  // confirms what criteria will be entered into the password
   capitalLetters = confirm("Would you like Capital Letters?");
   lowerCase = confirm("Would you like lowercase letters?");
   includeNumbers = confirm("Would you like numbers in your password?");
   specialCharacters = confirm("Would you like special characters?");
-
+  // based on results, pushes into the options array
   if (lowerCase) options.push(lowerCaseOp);
   if (capitalLetters) options.push(upperCaseOp);
   if (includeNumbers) options.push(numbersOp);
   if (specialCharacters) options.push(specialCharOp);
 
  
-
+// sets random index's for both options and then for the characterset so that it pulls randomly from first they options array, then randomly from the "options" inside of the options array.
   for (let index = 0; index < password_length; index++) {
     var characterSetIndex = Math.floor(Math.random() * options.length);
     var characterSet = options[characterSetIndex];
